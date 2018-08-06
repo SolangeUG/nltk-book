7218113729d97390967cac54751fbb107e6af5ad
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-06 14:13:12 +0200

    Extract text content from a web page

--- 03-processing-raw-text/exercises.py
+++ 03-processing-raw-text/exercises.py
@@ -1,4 +1,7 @@
 import re
+import json
+from urllib import request
+from bs4 import BeautifulSoup
 from nltk import word_tokenize
 from nltk.corpus import gutenberg
 
@@ -21,12 +24,54 @@ def print_wh_word_types():
     print(set(wh_words))
 
 
+def convert_to_celsius(temp):
+    """
+    Convert temperature from Farenheit to Celcius degrees
+    :param temp: input temperature in Farenheit
+    :return: equivalent temperature in Celsius
+    """
+    return ((temp - 32) * 5) / 9
+
+
+def print_weather_forecast():
+    """
+    Write code to access a favorite webpage and extract some text from it.
+    For example, access a weather site and extract the forecast top temperature for your town or city today.
+    :return: None
+    """
+    weather_url = "https://samples.openweathermap.org/data/2.5/weather" \
+                  "?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+    response = request.urlopen(weather_url)
+    page = response.read().decode('utf-8')
+
+    # extract text from the above page content
+    content = BeautifulSoup(page, features="html.parser").get_text()
+    # use json utilities to convert the json content to python dictionary
+    content = json.loads(content)
+
+    # extract desired information
+    weather = content['weather'][0]
+    print("Today's weather in London:", weather['description'])
+
+    # extract temperature measures
+    temp = float(content['main']['temp'])
+    print("Current temperature (°C):", "%.2f" % convert_to_celsius(temp))
+    min_temp = float(content['main']['temp_min'])
+    print("Minimum temperature (°C):", "%.2f" % convert_to_celsius(min_temp))
+    max_temp = float(content['main']['temp_max'])
+    print("Maximum temperature (°C):", "%.2f" % convert_to_celsius(max_temp))
+
+
 def main():
     """
     Main program entry
     :return: None
     """
+    print()
     print_wh_word_types()
+
+    print()
+    print_weather_forecast()
 
 
 if __name__ == '__main__':
