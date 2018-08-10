5ddec30d90e12403114b9a4a955e4c95a3f8dbad
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-10 22:35:52 +0200

    Complete anaylzer with linear regression and random forest regressor computations

--- 06-learning-to-classify-text/sentiment_intensity_analyzer.py
+++ 06-learning-to-classify-text/sentiment_intensity_analyzer.py
@@ -4,6 +4,9 @@ import json
 import numpy
 import urllib.request
 from nltk.sentiment.util import mark_negation
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from nltk.corpus import stopwords, opinion_lexicon
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -150,9 +153,30 @@ def extract_analyzer_features(dataset):
             # compute the mean over the columns
             feature_matrix[i, 0:3] = numpy.mean(sentence_polarities, axis=0)
             # compute the maximum over the columns
-            feature_matrix[i, 3:6] = numpy.max(sentence_polarities, axis=1)
+            feature_matrix[i, 3:6] = numpy.max(sentence_polarities, axis=0)
         else:
             feature_matrix[i, 0:6] = 0.0
+    return feature_matrix
+
+
+def get_additional_features(dataset):
+    """
+    Add two features to our training model:
+        (1) length of the review (in thousands of characters) - truncate at 2500
+        (2) percentage of exclamation marks (%)
+    :param dataset: input set of data
+    :return: a matrix with those features
+    """
+    feature_count = 2
+    feature_matrix = numpy.empty((len(dataset), feature_count))
+    for i in range(len(dataset)):
+        text = dataset[i]['reviewText']
+        feature_matrix[i, 0] = len(text) / 1000.
+        if text:
+            feature_matrix[i, 1] = 100. * text.count('!') / len(text)
+        else:
+            feature_matrix[i, 1] = 0.0
+    feature_matrix[feature_matrix > 2.5] = 2.5
     return feature_matrix
 
 
@@ -162,6 +186,9 @@ if __name__ == '__main__':
     # Download data and load into memory
     baby_dataset = load_data(datasetname, datadir)
     baby_train, baby_valid, baby_test = partition_train_validation_test(baby_dataset)
+    X_train_neg = dataset_to_matrix_with_negation(baby_train)
+    Y_train = dataset_to_targets(baby_train)
+    Y_valid = dataset_to_targets(baby_valid)
 
     # List of stop words in English
     english_stopwords = set(stopwords.words('english'))
@@ -184,3 +211,43 @@ if __name__ == '__main__':
     for sentence in sent_tokenize(sample_text):
         print("\n", sentence)
         print(analyzer.polarity_scores(sentence), "\n")
+
+    # Apply features to the training set
+    analyzer_train = extract_analyzer_features(baby_train)
+    # Additional features
+    additional_train = get_additional_features(baby_train)
+    # Let's see what these values look like
+    print(X_train_neg.shape, analyzer_train.shape, additional_train.shape)
+
+    # LINEAR REGRESSION ON TRAINING DATASET
+    # Stack training sets horizontally
+    X_train_augmented = numpy.concatenate((X_train_neg, analyzer_train, additional_train), axis=1)
+    # Use Linear Regression
+    linear_regression_augmented = LinearRegression().fit(X_train_augmented, Y_train)
+    # Compute prediction
+    pred_train_augmented = linear_regression_augmented.predict(X_train_augmented)
+    # Compute the mean absolute error
+    mae_train_augmented = mean_absolute_error(pred_train_augmented, Y_train)
+    print("Linear Regression | Mean absolute error on the training data is %f stars" % mae_train_augmented)
+
+    # RANDOM FOREST REGRESSION ON TRAINING DATASET
+    random_forest_augmented = RandomForestRegressor().fit(X_train_augmented, Y_train)
+    # Compute prediction
+    rf_pred_train_augmented = random_forest_augmented.predict(X_train_augmented)
+    # Compute the mean absolute error
+    rf_mae_train_augmented = mean_absolute_error(rf_pred_train_augmented, Y_train)
+    print("Random Forest Regressor | Mean absolute error on the training data is %f stars" % rf_mae_train_augmented)
+
+    # VALIDATION DATASET
+    X_valid_neg = dataset_to_matrix_with_negation(baby_valid)
+    analyzer_valid = extract_analyzer_features(baby_valid)
+    additional_valid = get_additional_features(baby_valid)
+    X_valid_augmented = numpy.concatenate((X_valid_neg, analyzer_valid, additional_valid), axis=1)
+
+    # LINEAR REGRESSION & RANDOM FOREST REGRESSOR ON VALIDATION DATASET
+    pred_valid_augmented = linear_regression_augmented.predict(X_valid_augmented)
+    rf_pred_valid_augmented = random_forest_augmented.predict(X_valid_augmented)
+    mae_valid_augmented = mean_absolute_error(pred_valid_augmented, Y_valid)
+    print("Linear Regression | On the validation set, we get %f error" % mae_valid_augmented)
+    rf_mae_valid_augmented = mean_absolute_error(rf_pred_valid_augmented, Y_valid)
+    print("Random Forest Regressor | On the validation set, we get  %f error" % rf_mae_valid_augmented)
