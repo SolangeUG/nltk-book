2900d6f81853d5dfdb282cbbb7e6b75b45d6fe71
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-11 08:25:55 +0200

    Remove externalized code

--- 06-learning-to-classify-text/sentiment_analyzer.py
+++ 06-learning-to-classify-text/sentiment_analyzer.py
@@ -1,93 +1,9 @@
-import os
-import gzip
-import json
-import urllib.request
-from nltk.corpus import stopwords
-from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import mark_negation
+# noinspection PyUnresolvedReferences
+from util import data_util
 from nltk.classify import NaiveBayesClassifier
+from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import extract_unigram_feats
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-
-def download_data(dataset_name, directory_name):
-    """
-    Download a given dataset name into a given directory
-    :param dataset_name: input dataset name
-    :param directory_name: input directory name
-    :return: None
-    """
-    filename = 'reviews_%s_5.json' % dataset_name
-    filepath = os.path.join(directory_name, filename)
-    if os.path.exists(filepath):
-        print("Dataset %s has already been downloaded to %s" % (dataset_name, directory_name))
-    else:
-        url = 'http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/%s.gz' % filename
-        urllib.request.urlretrieve(url, filepath + ".gz")
-        with gzip.open(filepath + ".gz", 'rb') as fin:
-            with open(filepath, 'wb') as fout:
-                fout.write(fin.read())
-        print("Downloaded dataset %s and saved it to %s" % (dataset_name, directory_name))
-
-
-def load_data(dataset_name, directory_name):
-    """
-    Load data in JSON format into memory
-    :param dataset_name: input dataset name
-    :param directory_name: input directory name
-    :return:
-    """
-    filepath = os.path.join(directory_name, 'reviews_%s_5.json' % dataset_name)
-    if not os.path.exists(filepath):
-        download_data(dataset_name, directory_name)
-    data = []
-    with open(filepath, 'r') as f:
-        for line in f:                            # read file line by line
-            item_hash = hash(line)                # we will use this later for partitioning our data
-            item = json.loads(line)               # convert JSON string to Python dict
-            item['hash'] = item_hash              # add hash for identification purposes
-            data.append(item)
-    print("Loaded %d data for dataset %s" % (len(data), dataset_name))
-    return data
-
-
-def partition_train_validation_test(data):
-    """
-    Partition a dataset into a training set, a validation set and a testing set
-    :param data: input dataset
-    :return: training, validation and testing set
-    """
-    # 60% : modulus is 0, 1, 2, 3, 4, or 5
-    data_train = [item for item in data if item['hash'] % 10 <= 5]
-    # 20% : modulus is 6 or 7
-    data_valid = [item for item in data if item['hash'] % 10 in [6, 7]]
-    # 20% : modulus is 8 or 9
-    data_test = [item for item in data if item['hash'] % 10 in [8, 9]]
-    return data_train, data_valid, data_test
-
-
-def tokenize_with_negation(text):
-    """
-    Split a text into lower-case tokens, removing all punctuation tokens and stopwords
-    :param text: input text
-    :return: lowercase word tokens, without punctuation or stopwords
-    """
-    # List of stop words in English
-    english_stopwords = set(stopwords.words('english'))
-    # Set of stopwords marked as negated
-    negated_stopwords = set(word + "_NEG" for word in english_stopwords)
-    # List of all stopwords, including negated words
-    all_stopwords = english_stopwords.union(negated_stopwords)
-
-    tokens = []
-    for sentence in sent_tokenize(text):
-        pretokens = word_tokenize(sentence.lower())
-        # exclude punctuation
-        pretokens = [token for token in pretokens if any(char.isalpha() for char in token)]
-        # exclude negated stop words (tagged as negated)
-        pretokens = mark_negation(pretokens)
-        tokens.extend(token for token in pretokens if token not in all_stopwords)
-    return tokens
+from nltk.tokenize import sent_tokenize
 
 
 def generate_labeled_documents(data, label):
@@ -101,7 +17,7 @@ def generate_labeled_documents(data, label):
     for i in range(len(data)):
         # we're interested in the 'reviewText' component of our dataset's elements
         documents.extend(
-            ((tokenize_with_negation(sent), label) for sent in sent_tokenize(data[i]['reviewText']))
+            ((data_util.tokenize_with_negation(sent), label) for sent in sent_tokenize(data[i]['reviewText']))
         )
     return documents
 
@@ -110,8 +26,8 @@ if __name__ == '__main__':
     # Dataset information
     datadir, dataset = './data/', 'Baby'
     # Download data and load into memory
-    baby_dataset = load_data(dataset, datadir)
-    baby_train, baby_valid, baby_test = partition_train_validation_test(baby_dataset)
+    baby_dataset = data_util.load_data(dataset, datadir)
+    baby_train, baby_valid, baby_test = data_util.partition_train_validation_test(baby_dataset)
 
     # Sentiment analyzer
     baby_train_docs_subj = generate_labeled_documents(baby_train[:800], 'subj')
